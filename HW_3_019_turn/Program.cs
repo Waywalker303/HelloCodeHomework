1c7f1d0cb44ac20eb72a460e57d76c77163e44cf
vasil <ip.vasiliy@bk.ru>
2022-06-14 19:38:02 +0400

    add homework example 3

---
+++ HW_3_019_turn/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да*/
+Console.WriteLine("Введите положительное пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number / 10000;
+int number2 = number / 1000 % 10; 
+int number4 = number % 100 / 10;
+int number5 = number % 10;
+if((number.ToString().Length) != 5)
+{
+    Console.WriteLine("Ошибка! Введите положительное пятизначное число");
+}
+else if (number1 == number5 && number2 == number4)
+{
+    Console.WriteLine("Число " + number + " Палиндром");
+}
+else
+{
+    Console.WriteLine("Число " + number + " не палиндром");
+}
+
