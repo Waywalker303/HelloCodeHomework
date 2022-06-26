d29a5aef2bf085c1976f6001fcc48472e168e05e
vasil <ip.vasiliy@bk.ru>
2022-06-26 13:44:47 +0400

    add HW example 6

---
+++ HW_6_041_arrayInputUser/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры числа через запятую. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+
+Console.Write("Введите числа через запятую: ");
+string userText = Console.ReadLine();
+//string userText = "0, 7, 8, -2, -2,-1, 1,-7, 567, 89, 223";
+userText = userText + ",";
+int[] array = new int[userText.Length];
+string text = String.Empty;
+int j = 0;
+
+for(int i = 0; i < userText.Length; i++)
+{
+    if(userText[i] != ',')
+    {
+        text = text + userText[i];
+    }
+    else
+    {
+        array[j] = Convert.ToInt32(text);
+        text = String.Empty;
+        j++;
+    }
+    
+}
+ArrayEven(array);
+
+void ArrayEven(int[] array)
+{
+int even = 0;
+for(int i = 0; i < array.Length; i++)
+{
+     if(array[i] > even )
+        {
+        even++;
+        }
+}
+Console.WriteLine($"Количество чисел больше нуля: {even}");
+}
---
+++ HW_6_043_lines_point/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, 
+                     y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) */
+
+double b1 = Input("Введите b1: ");
+double k1 = Input("Введите k1: ");
+double b2 = Input("Введите b2: ");
+double k2 = Input("Введите k2: ");
+
+ 
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+
+double Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToDouble(Console.ReadLine());
+}
